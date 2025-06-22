409fdde570143c22b06d64c34ffee370944cebf8
Josh Becker <beckerbi@gmail.com>
2025-06-22 16:04:40 -0400

    Follow redirects when downloading the latest data

--- src/ingest/download.ts
+++ src/ingest/download.ts
@@ -7,11 +7,11 @@ export default async function download() {
 
   if (!(await Bun.file('./data/dump.tsv').exists())) {
     console.log('Downloading and decompressing dump...')
-    await Bun.$`curl -s https://openlibrary.org/data/ol_dump_latest.txt.gz | gunzip -c > data/dump.tsv`
+    await Bun.$`curl -sL https://openlibrary.org/data/ol_dump_latest.txt.gz | gunzip -c > data/dump.tsv`
   }
 
   if (!(await Bun.file('./data/ratings.tsv').exists())) {
     console.log('Downloading and decompressing ratings...')
-    await Bun.$`curl -s https://openlibrary.org/data/ol_dump_ratings_latest.txt.gz | gunzip -c > data/ratings.tsv`
+    await Bun.$`curl -sL https://openlibrary.org/data/ol_dump_ratings_latest.txt.gz | gunzip -c > data/ratings.tsv`
   }
 }
